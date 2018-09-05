13cd65acdeadeed787b7993baeae8e538f2cc0e5
leomax2016 <leomax2016@mail.ru>
2018-09-05 21:04:40 +0400

    Initial commit

---
+++ onlineShop1/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using onlineShop1.Models;
+
+namespace onlineShop1.Controllers
+{
+  public class StoreController : Controller
+  {
+    // GET: Store
+    public ActionResult Index()
+    {
+      var category = new List<Category> {
+        new Category { Name = "Electronics"},
+        new Category { Name = "Furniture"},
+        new Category { Name = "Sport"},
+        new Category { Name = "Accessories"},
+      };
+      return View(category);
+    }
+    public ActionResult Browse(string category)
+    {
+      var categoryModel = new Category { Name = category };
+      return View(categoryModel);
+    }
+    public ActionResult Details(int id)
+    {
+      var item = new Item { Title = "item " + id };
+      return View(item);
+    }
+  }
+}
---
+++ onlineShop1/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace onlineShop1.Models
+{
+  public class Item
+  {
+    public string Title { get; set; }
+    public Category Category { get; set; }
+  }
+}
